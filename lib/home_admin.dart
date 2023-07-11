74f15af56446867e400d174917cb6af209038d92
Rog <rodrigo.gonzalez.olivares@alumnos.uta.cl>
2023-07-10 22:19:55 -0400

    admin_ro

--- lib/home_admin.dart
+++ lib/home_admin.dart
@@ -1,33 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:haf/purchases_history_page.dart';
+import 'package:haf/products_management_page.dart';
+import 'package:haf/settings_admin_page.dart';
 
 class HomeAdmin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Seleccionar Método de Compra'),
+        title: Text('Inicio (Admin)'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                // Lógica para seleccionar el método de compra con Junaeb
-                // Puedes agregar aquí la navegación a la siguiente página o la lógica adicional para Junaeb
-              },
-              child: Text('Pago con Junaeb'),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Lógica para seleccionar el método de compra con WebPay
-                // Puedes agregar aquí la navegación a la siguiente página o la lógica adicional para WebPay
-              },
-              child: Text('Pago con WebPay'),
-            ),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'assets/logo.png', // Ruta de la imagen (ajusta la ruta según la ubicación de tu imagen)
+            width: 200, // Ancho de la imagen
+            height: 200, // Alto de la imagen
+          ),
+          SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => PurchasesHistoryPage()),
+              );
+            },
+            child: Text('Historial de Pedidos'),
+          ),
+          SizedBox(height: 16),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (_) => ProductsManagementPage()),
+              );
+            },
+            child: Text('Gestión de Productos'),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (_) => SettingsAdminPage()),
+          );
+        },
+        child: Icon(Icons.settings),
       ),
     );
   }
---
+++ lib/settings_admin_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SettingsAdminPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ajustes de Administrador'),
+      ),
+      body: Center(
+        child: Text('Página de Ajustes de Administrador'),
+      ),
+    );
+  }
+}
